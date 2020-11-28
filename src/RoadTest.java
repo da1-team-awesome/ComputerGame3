87309dbda3a0f75bcb9613f24bac4a71da79da8c
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2020-11-28 15:49:47 +0100

    Refactor

--- src/RoadTest.java
+++ src/RoadTest.java
@@ -2,8 +2,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.*;
 
